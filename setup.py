f354a88383760e225586db1dc1c1059180fbfb01
Rakesh-46-VR <rakeshbsirvi46@gmail.com>
2025-01-22 23:26:06 +0530

    add auth module

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="auth",   # The name of your module
+    version="0.1",                 # Module version
+    packages=find_packages(),      # Automatically find all packages in the project
+)
