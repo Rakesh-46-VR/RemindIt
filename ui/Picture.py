4beb175b646dbb5895f480041088ee1d846ee177
Rakesh-46-VR <rakeshbsirvi46@gmail.com>
2025-01-23 17:06:01 +0530

    add logo from path or url

--- ui/Picture.py
+++ ui/Picture.py
@@ -1,6 +1,8 @@
 from PyQt6.QtWidgets import QLabel, QVBoxLayout, QWidget, QHBoxLayout
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtCore import Qt
+from io import BytesIO
+import requests
 
 class LogoWithText(QWidget):
     def __init__(self, logo_path: str, text: str, size, parent=None):
@@ -33,8 +35,9 @@ class LogoWithText(QWidget):
         # Set the layout
         self.setLayout(layout)
 
+
 class Logo(QWidget):
-    def __init__(self, logo_path: str, size, parent=None):
+    def __init__(self, logo_source: str, size, is_url=False, parent=None):
         super().__init__(parent)
 
         # Layout for the custom widget
@@ -42,7 +45,21 @@ class Logo(QWidget):
 
         # Add the logo
         logo_label = QLabel(self)
-        logo_label.setPixmap(QPixmap(logo_path))
+
+        # Load the logo (either from a file path or a URL)
+        pixmap = QPixmap()
+        if is_url:
+            # If the source is a URL, fetch the image data
+            response = requests.get(logo_source)
+            if response.status_code == 200:
+                image_data = BytesIO(response.content)
+                pixmap.loadFromData(image_data.read())
+        else:
+            # Otherwise, load from a local file path
+            pixmap.load(logo_source)
+
+        # Set the pixmap to the QLabel
+        logo_label.setPixmap(pixmap)
         logo_label.setScaledContents(True)
         logo_label.setFixedSize(size[0], size[1])
         layout.addWidget(logo_label)
